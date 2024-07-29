6539f53e38f45ebe7bf01d6d5a68c75c64c2b2fd
Abdulnasir Yakubu muhammad <132953212+Abdulnasirym@users.noreply.github.com>
2024-07-29 14:40:51 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,9 +9,6 @@ if __name__ == "__main__":
     url = https://jsonplaceholder.typicode.com/
     user = request.get("url + user/{}".format(sys.argv[1])).json()
     todos = request.get("url + todos/", params={"userId": sys.argv[1]}).json()
-
-    for task in todos:
-        if task.get('completed') is True:
-            completed.append(task.get("title"))
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
